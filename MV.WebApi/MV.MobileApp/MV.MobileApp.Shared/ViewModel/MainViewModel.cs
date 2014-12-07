8837600f12455839b77b77ad17c2d8af3999ef4c
JamesD <james.dejaegere@gmail.com>
2014-12-07 19:53:38 +0100

    blankapp created with template and mvvmlight added

---
+++ MV.WebApi/MV.MobileApp/MV.MobileApp.Shared/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MV.MobileApp.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _applicationName;
+
+        public string ApplicationName
+        {
+            get { return _applicationName; }
+            set { Set(() => ApplicationName, ref _applicationName, value); }
+        }
+        public MainViewModel()
+        {
+            ApplicationName = IsInDesignMode 
+                ? "Mobile Vikings" 
+                : "Mobile Vikings DESIGN";
+        }
+    }
+}
